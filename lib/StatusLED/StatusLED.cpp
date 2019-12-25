06fce13224be9748d0dc6d0e1bafa9ad9b2b8b46
flyingguitarman <flyingguitarman@gmail.com>
2019-12-25 00:54:58 -0600

    replaced delay with crono library

--- lib/StatusLED/StatusLED.cpp
+++ lib/StatusLED/StatusLED.cpp
@@ -1,24 +1,42 @@
 #include <Arduino.h>
 #include <StatusLED.h>
+#include <Chrono.h>
+
+int statusLEDState = LOW;
+
+Chrono statusLEDChrono;
 
 StatusLED::StatusLED(int pin)
 {
   pinMode(pin, OUTPUT);
   _pin = pin;
+  digitalWrite(_pin, statusLEDState);
 }
 
 void StatusLED::good()
 {
-  digitalWrite(_pin, HIGH);
-  delay(250);
-    digitalWrite(_pin, LOW);
-  delay(250);
+  if (statusLEDChrono.hasPassed(500)) {// returns true if it has been 500ms since it was started
+    statusLEDChrono.restart(); // restarts crono to trigger later.
+
+    // toggle stored state
+    if (statusLEDState==LOW) statusLEDState=HIGH;
+    else statusLEDState=LOW;
+
+    // writes new value to state and LED
+    digitalWrite(_pin, statusLEDState);    
+  }
 }
 
 void StatusLED::error()
 {
-  digitalWrite(_pin, HIGH);
-  delay(25);
-    digitalWrite(_pin, LOW);
-  delay(25);
+  if (statusLEDChrono.hasPassed(250)) {// returns true if it has been 500ms since it was started
+    statusLEDChrono.restart(); // restarts crono to trigger later.
+
+    // toggle stored state
+    if (statusLEDState==LOW) statusLEDState=HIGH;
+    else statusLEDState=LOW;
+
+    // writes new value to state and LED
+    digitalWrite(_pin, statusLEDState);    
+  }
 }
