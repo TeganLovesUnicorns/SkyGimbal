67afdedfcef9b5101a0073bb1ccdf6ab534faaa6
Daniel R <flyingguitarman@gmail.com>
2020-01-12 12:03:19 -0600

    Begin work on Units

---
+++ src/feature/Units/Units.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <Units.h>
+
+void Distance::feet() {  // 10" Circumference Wheel
+// 1 foot == 240 corse steps on 200 step stepper (direct drive)
+
+}
+
+void Distance::meters() { // 10" Circumference Wheel
+// 1 Meter == 3.28 feet ~ 787.2 steps
+
+}
---
+++ src/feature/Units/Units.h
@@ -0,0 +1,17 @@
+#ifndef Units_h
+  #define Units_h
+
+  #include "Arduino.h"
+
+  class Distance
+{
+private:
+//   int _travel;
+public:
+    // Distance(int travel);
+    void feet();
+    void meters();
+};
+
+
+#endif
