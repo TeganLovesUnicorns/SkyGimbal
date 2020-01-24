3e81b9f8ff4c3ed548e6fea28004fbc934d6c62c
Daniel R <flyingguitarman@gmail.com>
2020-01-24 10:12:41 -0600

    commented out units library for comp purposes

--- src/feature/Units/Units.h
+++ src/feature/Units/Units.h
@@ -1,17 +1,17 @@
-#ifndef Units_h
-  #define Units_h
+// #ifndef Units_h
+//   #define Units_h
 
-  #include "Arduino.h"
+//   #include "Arduino.h"
 
-  class Distance
-{
-private:
-//   int _travel;
-public:
-    // Distance(int travel);
-    void feet();
-    void meters();
-};
+//   class Distance
+// {
+// private:
+// //   int _travel;
+// public:
+//     // Distance(int travel);
+//     void feet();
+//     void meters();
+// };
 
 
-#endif
+// #endif
